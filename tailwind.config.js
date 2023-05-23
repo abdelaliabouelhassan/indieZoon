11fcbc6bc149f5cd7ea24ba7dc22678242cc432a
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-05-23 21:17:54 +0100

    changed the favicon icon and added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    content: [
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+    ],
+    theme: {
+      extend: {
+        screens: {
+          'xs': '475px',
+        },
+        
+        colors:{
+          'primary':'#fed700',
+        //   'off-white':'#f9f9f9',
+  
+        //   'light-grey':'#f4f4f4',
+          
+        //   'success':'#2D9163',
+        //   'danger':'#DE4654',
+        //   'warning':'#FFC619',
+        //   'info':'#1CCDF1',
+  
+  
+        //   'light':'#555555', //for text
+        //   'extra-light':'#a4a4a4', //for text
+        },
+        
+        
+      },
+    },
+    plugins: [],
+  }
+  
+  
