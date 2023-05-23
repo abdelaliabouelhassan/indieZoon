6d94330517795a734959c9fc75f6620b2c8905aa
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-05-23 21:36:41 +0100

    fixed setup bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-    app: {
-        head: {
-          charset: 'utf-8',
-          viewport: 'width=device-width, initial-scale=1',
-          link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-          ],
-        }
-    },
-    pages: true,
-    modules: [
-      '@nuxtjs/tailwindcss'
-    ]
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
