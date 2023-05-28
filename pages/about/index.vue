fcf9f484d4e878af11f57e81e2440faca10ba6da
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-05-28 16:49:22 +0100

    working on the team section, and animation is smooth, the image sizes are set, working on the team member info

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=" bg-primary/5 h-[100rem]">
-        <UIBaseHoverInfo><template #sl >rrrrrrrrrrr</template></UIBaseHoverInfo>
+        <PartialsSectionsAboutUsFirstSection />
     </div>
 </template>
 
