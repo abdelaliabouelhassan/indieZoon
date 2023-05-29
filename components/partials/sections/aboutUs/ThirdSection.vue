e0a87fee922ec58025eb93bb31ef263292e40033
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-05-29 16:14:01 +0100

    finised the top two articles in the third section of about us

--- components/partials/sections/aboutUs/ThirdSection.vue
+++ components/partials/sections/aboutUs/ThirdSection.vue
@@ -1,63 +1,75 @@
 <template>
-    <section class="pb-36">
+    <section class="pb-36 overflow-hidden">
         <h2 class="text-4xl font-bold sm:text-5xl text-center">Unsere Mission</h2>
         <!-- articles -->
-        <div class="flex flex-col mt-20 gap-y-20">
+        <div class="flex flex-col mt-20 ">
             <!-- article 1 -->
-            <article class=" relative">
-                <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+            <!-- h-[34rem] xs:h-[30rem] sm:h-[25rem] md:h-[20rem] -->
+            <article class="clipable z-20 bg-primary pt-32 pb-48 xs:pt-24 xs:pb-44 sm:pt-16 md:pt-10 md:pb-32">
+                <div class=" text-white md:flex md:justify-end lg:max-w-[69.375rem] mx-auto">
+                    <div class="h-full px-4  flex flex-col gap-y-4 
+                                 sm:max-w-[40.2rem] md:max-w-[38rem] lg:max-w-[45rem]">
                         <h2 class="text-4xl font-bold sm:text-5xl">Planung und Konzept</h2>
                         <p class="font-light text-sm">In Zusammenarbeit mit verschiedenen Interessengruppen entwickeln wir eine maßgeschneiderte Plattform, die den Fokus auf bequemes, nachhaltiges Leben und lokale Angebote legt. Unser Ziel: Gemeinsam den ökologischen Fußabdruck und den unserer Städte reduzieren.</p>
                     </div>
                 </div>
-                <img class="absolute w-48 left-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city1.png" alt="city1">
             </article>
+            <div id="article1_images" class="container mx-auto relative ">
+                <img id="city-img1" class="absolute w-48 left-0 bottom-2 translate-y-1/2 z-30 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3
+                            md:buttom-[13.4rem]" src="/images/about_us/third_section/city1.png" alt="city1">
+                <img id="article1-tree1" class="absolute hidden md:block w-16 left-12 -bottom-28" src="/images/about_us/third_section/tree8.png" alt="tree8">
+                <img id="article1-tree2" class="absolute hidden md:block w-16 right-20 -bottom-40" src="/images/about_us/third_section/tree5.png" alt="tree5">
+                <img id="road-1" class="absolute hidden md:block z-10 w-[74%] top-6 left-[11.5rem]" src="/images/about_us/third_section/road1.svg" alt="road1">
+            </div>
             <!-- article 2 -->
-            <article class="mt-20  relative">
-                <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+            <article id="article2" class="clipable mt-40 z-20 bg-primary/10 pt-32 pb-48 xs:pt-24 xs:pb-44 sm:pt-16 md:pt-10 md:pb-20
+                md:mt-44">
+                <div class=" text-white lg:max-w-[69.375rem] mx-auto">
+                    <div class="h-full px-4 flex flex-col gap-y-4
+                                 sm:max-w-[40.2rem] md:max-w-[38rem] lg:max-w-[45rem]">
                         <h2 class="text-4xl font-bold sm:text-5xl">Challenge 1: Lokaler online-Marktplatz </h2>
                         <p class="font-light text-sm">Unser lokaler online-Marktplatz verbindet Kaufer:innen mit regionalen Einzelhandlern und Handlerinnen, urn damit lokale Produktion statt internationaler Importe zu fordern Damit entfallt der klimaschadliche Versand und die Retouren, Versandmitarbeiter:innen werden nicht ausgebeutet und lokale Produktionen werden sichtbar. Auch mochten wir lokale Dienstleister:innen integrieren, urn lokale Fachkrafte zu fordern. So tragen wir zu einer nachhaltigen Innenstadtentwicklung und einer Weiterentwicklung der lokalen Wirtschaft bei. </p>
                     </div>
                 </div>
-                <img class="absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city2.png" alt="city2">
             </article>
+            <div id="article2_images" class="container mx-auto relative z-30">
+                <img id="city-img2" class=" absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city2.png" alt="city2">
+            </div>
             <!-- article 3 -->
-            <article class="mt-20  relative">
+            <article class="mt-40  relative z-20">
                 <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+                    <div class="h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
                         <h2 class="text-4xl font-bold sm:text-5xl">Challenge 2: Veranstaltungen  </h2>
                         <p class="font-light text-sm">IndieZone bietet ein zentrales Veranstaltungsportal fur lokale Events. Hier konnen Veranstaltungen in der Umgebung entdeckt, lokale Kunstlerinnen unterstutzt und das soziale Leben in der Stadt aktiv mitgestaltet werden. Wir ermoglichen es jedem und jeder, schnell und unkompliziert eigene Veranstaltungen uber unsere Plattform online bekannt zu machen. Ob Jazz-Abende in der Kellerbar nebenan, die Vernissage einer Kunststudentin oder offizielle Veranstaltungen der Stadt: Hier findet alles seinen Platz und du diese Veranstaltung in deiner Umgebung.  </p>
                     </div>
                 </div>
-                <img class="absolute w-48 left-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city3.png" alt="city3">
+                <img class="city-img absolute w-48 left-0 bottom-2 translate-y-1/2 z-30 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city3.png" alt="city3">
             </article>
             <!-- article 4 -->
-            <article class="mt-20  relative">
+            <article class="mt-20  relative z-20">
                 <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+                    <div class="h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
                         <h2 class="text-4xl font-bold sm:text-5xl">Challenge 3: Vereine und Projekte  </h2>
                         <p class="font-light text-sm">Wir integrieren Vereine, Projekte und gemeinnutzige Organisationen in unsere Plattform, urn das soziale Zusammenleben zu fordern Hier kann man Vereine aus der Umgebung finden, urn neue soziale Kontakte zu knupfen. Auch warten spannende Projekte auf neue Mitarbeiter:innen und lokale Initiativen freuen sich auf engagierte Mitglieder:innen. So begegnen sich die Menschen wieder in lebendigen und nachhaltigen Innenstadten. </p>
                     </div>
                 </div>
-                <img class="absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city4.png" alt="city4">
+                <img class="city-img absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city4.png" alt="city4">
             </article>
             <!-- article 5 -->
-            <article class="mt-20  relative">
+            <article class="mt-20  relative z-20">
                 <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+                    <div class="h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
                         <h2 class="text-4xl font-bold sm:text-5xl">Challenge 4: Wissen   </h2>
                         <p class="font-light text-sm">IndieZone widmet sich der Bereitstellung von umfangreichen, informativen Ressourcen und praxisnahen Tipps rund urn das Thema nachhaltiges Leben. Unser Blog bietet wertvolle Einblicke in die neuesten Entwicklungen und Best Practices in den Bereichen Umwelt- und Sozialverantwortung. Zusatzlich bieten wir Online-Kurse an, in denen Experten und Expertinnen ihr Bewohnerinnen uber helfen, nachhaltiger zu handeln. 
                         Wissen zu Nachhaltigkeitsthemen teilen. So konnen wir all Einzelhandlern, Vereinen und Veranstalter*innen bi- </p>
                     </div>
                 </div>
-                <img class="absolute w-48 left-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city5.png" alt="city5">
+                <img class="city-img absolute w-48 left-0 bottom-2 translate-y-1/2 z-30 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" src="/images/about_us/third_section/city5.png" alt="city5">
             </article>
             <!-- article 6 -->
-            <article class="mt-20  relative">
+            <article class="mt-20  relative z-20">
                 <div class="bg-primary text-white clipable  relative ">
-                    <div class="container mx-auto h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
+                    <div class="h-full px-4 pt-32 pb-48 flex flex-col gap-y-4">
                         <h2 class="text-4xl font-bold sm:text-5xl">Noch mehr Ideen?    </h2>
                         <p class="font-light text-sm">Beteilige dich an unserer Mission, das nachhaltige Leben einfach zu machen. Teile deine Ideen und Vorschlage mit uns, um gemeinsam eine umweltfreundliche und zukunftsfahige Stadtlandschaft zu gestalten.  </p>
                         <UIBaseButton class="px-10 py-4 bg-white mt-8 hover:scale-y-110 hover:scale-x-110 self-center">
@@ -65,7 +77,7 @@
                         </UIBaseButton>
                     </div>
                 </div>
-                <img class="absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" 
+                <img class="city-img absolute w-48 right-0 bottom-2 translate-y-1/2 xs:w-56 xs:bottom-2.5 sm:w-64 sm:bottom-3" 
                 src="/images/about_us/third_section/city5.png" alt="city5">
             </article>
         </div>
@@ -94,7 +106,7 @@ onMounted(() => {
 
 <style scoped>
 .clipable{
-    /* clip-path: inset(0 0 0 95%); */
+    clip-path: inset(0 0 0 95%);
     transition-property: clip-path;
     transition-duration: 1s;
     transition-timing-function: ease-in-out;
@@ -102,5 +114,53 @@ onMounted(() => {
 .full{
     clip-path: inset(0 0 0 0);
 }
-
+@media (min-width: 53.1rem) {
+    #city-img1{
+        width:22rem;
+        bottom: 1rem;
+    }
+    #article1-tree1{
+        width: 6rem;
+        left: 4rem;
+        bottom: -9rem;
+    }
+    #article1-tree2{
+        width: 6rem;
+        right: 6rem;
+        bottom: -14rem;
+    }
+    #road-1{
+        width: 75%;
+        top: 5rem;
+        left: 13rem;
+        transform: rotate(8deg);
+        clip-path: inset(0 0 12% 0);
+    }
+    #article2{
+        margin-top: 20rem;
+    }
+    #city-img2{
+        width:22rem;
+        bottom: 1rem;
+    }
+}
+@media (min-width: 58.5rem) {
+    #road-1{
+        clip-path: inset(0 0 10% 0);
+        width: 73%;
+        top: 4rem;
+        left: 14rem;
+        transform: rotate(3deg);
+    }
+}
+@media (min-width: 64rem) {
+    #road-1{
+        clip-path: inset(0 0 0% 0);
+        aspect-ratio: 0.8;
+        width: 72%;
+        top: -8.5rem;
+        left: 17rem;
+        transform: rotate(-6deg);
+    }
+}
 </style>
