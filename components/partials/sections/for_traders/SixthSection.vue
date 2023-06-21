4f35be54a637274a001debe2ad66fe7be86a4847
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2023-06-21 16:30:12 +0100

    for traders trans

--- components/partials/sections/for_traders/SixthSection.vue
+++ components/partials/sections/for_traders/SixthSection.vue
@@ -15,14 +15,14 @@
         <div class="w-full container py-48 z-[2] mx-auto px-4 flex flex-col items-center text-white">
             <!-- section header -->
             <h2 class="text-4xl font-bold sm:text-5xl text-center px-4 sm:mt-44">
-                <span class="md:hidden">FAQ</span> 
-                <span class="hidden md:block">Häufige Fragen (FAQ)</span> 
+                <span class="md:hidden">{{ $t('FAQ') }}</span> 
+                <span class="hidden md:block">{{ $t('Häufige_Fragen') }}</span> 
             </h2>
             <!-- accorion elements -->
             <div v-accordion class="accordion flex flex-col gap-y-5 mt-20  max-w-2xl">
                 <div class="accordion-item">
                     <div class="accordion-header px-4 py-2 bg-white text-primary font-semibold rounded-md flex items-center justify-between">
-                        <div> Muss ich Produkte hochladen? </div>
+                        <div> {{ $t('Produkte') }} </div>
                         <svg class="chevron -rotate-90 w-3 h-3" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129">
                             <g>
                                 <path opacity="0.8" d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
@@ -30,12 +30,12 @@
                         </svg>
                     </div>
                     <div class="accordion-content">
-                        <p class="p-4">IndieZone bietet drei verschiedene Abomodelle an. Das günstigste Modell beinhaltet eine reine Shoppräsenz auf unserer Plattform. Das EInstellen von Produkten ist somit keine Pflicht. Wir würden es Ihnen aber sehr raten, um den Kunden ein möglichst gutes Bild von dem zu geben, was sie in Ihrem Laden kaufen können.</p>
+                        <p class="p-4">{{ $t('IndieZone_bietet_drei') }}</p>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <div class="accordion-header px-4 py-2 bg-white text-primary font-semibold rounded-md flex items-center justify-between">
-                        <div> Was kostet IndieZone? </div>
+                        <div> {{ $t('Was_kostet_IndieZone') }} </div>
                         <svg class="chevron -rotate-90 w-3 h-3" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129">
                             <g>
                                 <path opacity="0.8" d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
@@ -43,12 +43,12 @@
                         </svg>
                     </div>
                     <div class="accordion-content">
-                        <p class="p-4">Anders als die Onlinemarktplatzgiganten wird IndieZone keine Margen verlangen. Wir werden ein Abomodell verfolgen, welches sich nicht an Ihren Umsätzen, sondern an der eingestellten Produktanzahl bemisst. Da wir noch nicht auf dem Markt sind, stehen unsere Preise noch nicht fest. Die verschiedenen Abomodelle werden sich jedoch vorraussichtlich zwischen 9€ und 100€ befinden.<br> Derzeit sind wir mit verschiedenen Städten im Gespräch, um unseren Händlern einen kostenlosen Zugang zu ermöglichen.</p>
+                        <p class="p-4">{{ $t('Anders_als') }}<br> {{ $t('Derzeit') }}</p>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <div class="accordion-header px-4 py-2 bg-white text-primary font-semibold rounded-md flex items-center justify-between">
-                        <div> Ist die Voranmeldung verbindlich? </div>
+                        <div> {{ $t('Voranmeldung') }} </div>
                         <svg class="chevron -rotate-90 w-3 h-3" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129">
                             <g>
                                 <path opacity="0.8" d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
@@ -56,27 +56,27 @@
                         </svg>
                     </div>
                     <div class="accordion-content">
-                        <p class="p-4">Die Voranmeldung bei IndieZone ist nicht verbindlich, sichert Ihnen aber einen Platz, sobald wir in Ihrer Region auf den Markt gehen. Zudem sagen wir Ihnen natürlich Bescheid, sobald IndieZone online geht. Außerdem unterstützen Sie uns durch eine Voranmeldung, da wir durch das so gezeigte Interesse bei Städten für eine Förderung Ihres Abonements argumentieren können.</p>
+                        <p class="p-4">{{ $t('Die_Voranmeldung') }}</p>
                     </div>
                 </div>
             </div>
             
             <div class="flex flex-col items-center gap-y-5 mt-20  xl:mt-24">
-                <div class="text-center md:text-xl lg:text-2xl md:font-semibold">Noch nicht alle Fragen beantwortet?</div>
+                <div class="text-center md:text-xl lg:text-2xl md:font-semibold">{{ $t('Noch_nicht') }}</div>
                 <nuxt-link to="/contact">
                     <UIBaseButton class="px-10 py-4 bg-white hover:scale-y-125 hover:scale-x-110">
-                        <div class="text-primary">Jetzt direkt Fragen</div> 
+                        <div class="text-primary">{{ $t('Jetzt_direkt_Fragen') }}</div> 
                     </UIBaseButton>
                 </nuxt-link>
             </div>
 
             <div class="flex flex-col items-center gap-y-5 text-center mt-48   ">
-                <div class="font-light">Werde Teil der IndieZone</div>
-                <p class="text-xl font-medium xs:text-2xl sm:text-3xl  sm:max-w-2xl lg:max-w-4xl">Registriere dich direkt und mache den nächsten Schritt. Werde Teil der wachsenden Gemeinschaft und gestalte den Einzelhandel der Zukunft.<br><br>Einfach. Fair. Effektiv.</p>
+                <div class="font-light">{{ $t('Werde_Teil_der_IndieZone') }}</div>
+                <p class="text-xl font-medium xs:text-2xl sm:text-3xl  sm:max-w-2xl lg:max-w-4xl">{{ $t('Registriere') }}<br><br>{{ $t('Einfach_Fair') }}</p>
             </div>
             <nuxt-link to="/pre-register" class="mt-20">
                 <UIBaseButton class="px-10 py-4 bg-white   hover:scale-y-125 hover:scale-x-110">
-                    <div class="text-primary"> Jetzt Voranmelden</div> 
+                    <div class="text-primary"> {{ $t('Jetzt_Voranmelden') }}</div> 
                 </UIBaseButton>
             </nuxt-link>
         </div>
--- components/partials/sections/for_traders/ThirdSection.vue
+++ components/partials/sections/for_traders/ThirdSection.vue
@@ -14,7 +14,7 @@
         <!-- articles container -->
         <div class="z-[2] py-32 lg:pt-48 xl:pt-10 lg:max-w-[69.375rem] lg:mx-auto">
             <!-- section head title -->
-            <h2 class="text-4xl font-bold sm:text-5xl text-center px-4 pt-32 lg:pt-36 xl:pt-56">Warum IndieZone?</h2>
+            <h2 class="text-4xl font-bold sm:text-5xl text-center px-4 pt-32 lg:pt-36 xl:pt-56">{{ $t('Warum_IndieZone') }}</h2>
             <!-- first article -->
             <article class="w-full flex flex-col gap-y-16 pt-32 relative
                             md:pt-44  md:items-center
@@ -34,9 +34,9 @@
                             md:order-1 md:max-w-xl w-full
                             lg:order-2 lg:w-[50%] lg:max-w-none lg:ml-5">
                     <!-- article title -->
-                    <h3 class="text-3xl font-bold sm:text-4xl">Werde gefunden</h3>
+                    <h3 class="text-3xl font-bold sm:text-4xl">{{ $t('Werde_gefunden') }}</h3>
                     <!-- article paragraph -->
-                    <p class="font-light">Unser Marktplatz verbindet lokale Läden und Geschäfte in deiner Umgebung. Neue Kunden werden deinen Laden und deine Produkte endlich finden und zwar da wo sie suchen: online. Mit IndieZone als Partner erreichst du ohne Vorkenntnisse und großen Aufwand neue Kundinnen und ergänzt deine lokale Präsenz gezielt online. Die Kunden kaufen dabei trotzdem vor Ort bei dir im Laden.</p>
+                    <p class="font-light">{{ $t('Unser') }}</p>
                     
                 </div>
             </article>
@@ -57,9 +57,9 @@
                             md:order-1 md:max-w-xl 
                             lg:text-left lg:order-2 lg:w-[50%] lg:max-w-none">
                     <!-- article title -->
-                    <h3 class="text-3xl font-bold sm:text-4xl">Verbinde Tradition mit Innovation</h3>
+                    <h3 class="text-3xl font-bold sm:text-4xl">{{ $t('Verbinde_Tradition') }}</h3>
                     <!-- article paragraph -->
-                    <p class="font-light lg:max-w-[27.688rem]">Mit IndieZone bringst du die Stärke deines lokalen Ladens in die digitale Welt. Wir zeigen der Welt was du zu bieten hast. Dabei musst du das persönliche Verkaufen nicht aufgeben, um die Vorteile eines online-Marktplatzes zu nutzen: Weil sich Versand nicht lohnt - vergiss ihn. Du machst, was du am liebsten tust, und teilst deine Leidenschaft mit Menschen die das zu schätzen wissen. Denn dafür kommen die Kunden in deinen Laden.</p>
+                    <p class="font-light lg:max-w-[27.688rem]">{{ $t('Mit_IndieZone') }}</p>
                 </div>
                 <!-- image space allocator -->
                 <div class="hidden lg:block w-[50%] lg:order-2"></div>
@@ -83,9 +83,9 @@
                             md:order-1 md:max-w-lg 
                             lg:order-2 lg:w-[50%] lg:max-w-none lg:ml-28">
                     <!-- article title -->
-                    <h3 class="text-3xl font-bold sm:text-4xl">Erreiche Großes</h3>
+                    <h3 class="text-3xl font-bold sm:text-4xl">{{ $t('Erreiche_Großes') }}</h3>
                     <!-- article paragraph -->
-                    <p class="font-light lg:max-w-[27.875rem]">IndieZone bietet dir mehr als nur eine Online-Präsenz. Als All-in-One-Marketing Lösung nehmen wir dir alles ab, was dich im Marketing bisher frustriert hat. Marketing ist teuer, online-Marketing noch teurer und gegen Amazon hat man trotzdem keine Chance? Nicht mit uns, dank unserer fairen Abomodelle. Werde Teil der IndieZone-Community und profitiere von Gemeinschaftsprojekten und unserem integrierten Blog, um immer auf dem neuesten Stand zu bleiben.</p>
+                    <p class="font-light lg:max-w-[27.875rem]">{{ $t('IndieZone_bietet') }}</p>
                 </div>
             </article>
             
@@ -94,7 +94,7 @@
         <div id="third_setion_button" class="w-full  h-[5.5rem] pb-2 z-[2] absolute bottom-0   flex items-center justify-center  ">
             <nuxt-link to="/pre-register">
                 <UIBaseButton class="w-max py-4  hover:scale-y-125 hover:scale-x-110">
-                        Jetzt Voranmelden
+                        {{ $t('Jetzt_Voranmelden') }}
                 </UIBaseButton>
             </nuxt-link>
         </div>
